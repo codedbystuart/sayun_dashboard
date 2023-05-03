a25b865cb4434974a5f02ab6228cc6c8848499e3
dambi <dambi.stuart@builder.ai>
2023-05-03 13:32:04 +0300

    initial release

---
+++ src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Rogers Sekamate',
+  email: 'rogers@sayun.coffee',
+  photoURL: '/assets/images/avatars/avatar_default.jpg',
+};
+
+export default account;
